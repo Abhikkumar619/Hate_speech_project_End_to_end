74db95a8ba7cff0427fabb2d41dc534ed0568b0e
Abhikkumar619 <abisheky194@gmail.com>
2024-04-19 12:00:39 +0545

    setup completed

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Hate_speech_project_End_to_end
+
+## HOW TO RUN ?
+```bash
+conda create -p hate_env python=3.8 -y
+```
+
+```bash
+conda activate hate_env
+```
+
+```bash
+pip install -r requirement.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Hate_speech_classification",
+    version="0.0.1",
+    author="Abishek",
+    email="abisheky194@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
